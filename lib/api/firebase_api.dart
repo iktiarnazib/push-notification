875a4e5767a9b20af4bc62240d0602003297ee18
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-20 19:58:56 +0600

    On notification clicked, what to do

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:pushnotification/main.dart';
+import 'package:pushnotification/pages/notification_page.dart';
 
 class FirebaseApi {
   //create a firebase instance
@@ -14,5 +17,26 @@ class FirebaseApi {
 
     //print user token
     print("token: ${fCMToken}");
+  }
+
+  //Funciton to handle received message
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) {
+      return;
+    }
+
+    navigatorKey.currentState?.pushNamed(
+      'notificationScreen',
+      arguments: message,
+    );
+  }
+
+  //Function to initialize background settings
+  Future initPushNotification() async {
+    //handle notification if the app was terminated and now opened
+    firebaseMessaging.getInitialMessage().then(handleMessage);
+
+    //attach event listeners for when a notification opens the app
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:pushnotification/api/firebase_api.dart';
 import 'package:pushnotification/pages/home_page.dart';
+import 'package:pushnotification/pages/notification_page.dart';
 import 'firebase_options.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +19,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      navigatorKey: navigatorKey,
+      routes: {'notificationScreen': (context) => NotificationPage()},
+    );
   }
 }
